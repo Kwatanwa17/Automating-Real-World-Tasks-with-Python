18074913ff5fe005845173b5472fc759c712f5dc
Kwatanwa17 <kwatanwa17@gmail.com>
2020-09-13 15:12:12 +0900

    Complete cars.py

--- Automatically Generate a PDF and send it by Email/cars.py
+++ Automatically Generate a PDF and send it by Email/cars.py
@@ -3,6 +3,9 @@
 import json
 import locale
 import sys
+import os
+import reports
+import emails
 
 
 def load_data(filename):
@@ -47,14 +50,13 @@ def process_data(data):
             year_sales[car_year] = item["total_sales"]
 
     # get max key and value
-
     max_year, max_total_sales = max(year_sales.items(), key=lambda x: x[1])
 
     summary = [
         "The {} generated the most revenue: ${}".format(
             format_car(max_revenue["car"]), max_revenue["revenue"]),
         "The {} had the most sales: {}".format(
-           format_car(max_model_sales["car"]), max_model_sales["total_sales"]),
+            format_car(max_model_sales["car"]), max_model_sales["total_sales"]),
         "The most popular year was {} with {} sales.".format(
             max_year, max_total_sales)
     ]
@@ -75,9 +77,21 @@ def main(argv):
     data = load_data("car_sales.json")
     summary = process_data(data)
     print(summary)
-    # TODO: turn this into a PDF report
+    filename = "./tmp/report.pdf" if os.name == "nt" else "/tmp/report.pdf"
 
-    # TODO: send the PDF report as an email attachment
+    # turn this into a PDF report
+    additional_info = "<br/>".join(summary)
+    table_data = cars_dict_to_table(data)
+    reports.generate(filename, "Sales summary for last month", additional_info, table_data)
+
+    # send the PDF report as an email attachment
+    sender = "automation@example.com"
+    receiver = "{}@example.com".format(os.environ.get('USER'))
+    subject = "Sales summary for last month"
+    body = "\n".join(summary)
+
+    message = emails.generate(sender, receiver, subject, body, filename)
+    emails.send(message)
 
 
 if __name__ == "__main__":
